5cdcadcb9c55b7969172be85f313341a1d68dc8f
DuloeW <duloekeren@gmail.com>
2025-02-28 06:57:35 +0800

    remove controller basic

--- src/controller/controller.ts
+++
@@ -1,9 +0,0 @@
-export function getAllData(c: any) {
-    return c.json({ message: 'Hello, World!' })
-}
-
-export async function addData(c: any) {
-    const body = await c.req.json()
-    const data = { ...body }
-    return c.json(data?.data)
-}
