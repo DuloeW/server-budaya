02132c394791a3f4afcbcf63d2be35e26e1b9bd2
DuloeW <duloekeren@gmail.com>
2025-02-27 16:48:45 +0800

    try making login but still got error in .env

--- src/routes/route.ts
+++ src/routes/route.ts
@@ -1,38 +1,54 @@
 import { Hono } from 'hono'
 import { prettyJSON } from 'hono/pretty-json'
 import { addData, getAllData } from '../controller/controller.js'
-import { payload, type Variables } from '../utils/tool.js'
+import { expiredIn, payload, type Variables } from '../utils/tool.js'
 import { jwt } from 'hono/jwt';
 import { sign } from 'hono/jwt';
+import { logger } from 'hono/logger';
+import { basicAuth } from 'hono/basic-auth';
+import { use } from 'hono/jsx';
+import { zValidator } from '@hono/zod-validator'
+import { z } from 'zod'
+import { HTTPException } from 'hono/http-exception';
+import { setCookie } from 'hono/cookie';
 
 const app = new Hono<{Variables: Variables}>
 
 app.use(prettyJSON())
+app.use(logger())
 
+const BASE_URL = '/api'
+const BASE_URL_AUTH = '/api/auth'
+const TOKEN = process.env.TOKEN!
 
-app.use(
-    '/auth/*',
+//MIDDLEWARE
+app.use(`${BASE_URL_AUTH}/*`, 
     jwt({
-        secret: 'secret',
+        secret: "akakak"
     })
 )
 
-app.get('/auth/page', (c) => {
-    const payload = c.get('jwtPayload')
-    return c.json({ message: 'Hello, World!', payload })
+app.get(`${BASE_URL_AUTH}/`, (c: any) => getAllData(c))
+
+app.post(`${BASE_URL_AUTH}/cuki/add`, (c: any) => addData(c))
+
+app.get(`${BASE_URL_AUTH}/products`, (c: any) => {
+    return c.json({ message: 'List message!' })
 })
 
 
-app.get('/login', (c) => {
-    const token = payload('admin', 'admin', 60)
-    return c.json(token)
+// DONT USE MIDDLERWARE
+app.post(`${BASE_URL}/login`, async (c: any) => {
+    const {username, password} = await c.req.json()
+     
+    const secreet = process.env.JWT_SECRET_KEY!
+
+    if(username !== 'admin' || password !== 'admin') {
+        throw new HTTPException(401, { message: 'Invalid username or password' })
+    } 
+    const token = await sign(payload(username, expiredIn), secreet || 'kontol')
+    setCookie(c, 'token', token)
+    return c.json({ token })
 })
-
-
-
-
-app.get('/', (c) => getAllData(c))
-
-app.post('/cuki/add', (c) => addData(c))
 
 export default app
--- src/utils/tool.ts
+++ src/utils/tool.ts
@@ -3,11 +3,11 @@ import type { JwtVariables } from "hono/jwt";
 export const SECREET = 'testing'
 export type Variables = JwtVariables;
 
-export const payload = (sub: string, role: string, exp: number) => {
+export const expiredIn = Math.floor(Date.now() / 1000) + (86400 * 7);
+
+export const payload = (name: string, exp: number) => {
     return {
-        sub,
-        role,
-        exp: Math.floor(Date.now() / 1000) + 60 * exp,
+        name,
+        exp
     }
 }
-
