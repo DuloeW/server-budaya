3abcb5b1d3830e847e7d85ea4b26a3c307e668f6
DuloeW <duloekeren@gmail.com>
2025-02-28 22:43:33 +0800

    change type 'c' from any to Context

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,8 +1,8 @@
-import { Hono } from "hono";
+import { Hono, type Context } from "hono";
 import { getAllUsers } from "../controller/user/user.controller.js";
 
 const userRoute = new Hono()
 
-userRoute.get(`/get-all/users`, async (c: any) => getAllUsers(c))
+userRoute.get(`/get-all/users`, async (c: Context) => getAllUsers(c))
 
 export default userRoute
