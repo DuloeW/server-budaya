331c1dc07cebd77b3fcef000a9a92a812919b61a
DuloeW <duloekeren@gmail.com>
2025-02-28 15:29:00 +0800

    change the directory

--- src/db/category.model.ts
+++
@@ -1,8 +0,0 @@
-import { mysqlTable, serial, text, varchar } from "drizzle-orm/mysql-core";
-
-export const category = mysqlTable('ketegori_budaya', {
-    id: serial('id').primaryKey(),
-    title: varchar('title', {length: 50}).notNull(),
-    description: text('description').notNull(),
-    image_url: varchar('image_url', {length: 255}).notNull(),
-})
--- src/db/summary.model.ts
+++
@@ -1,7 +0,0 @@
-import { mysqlTable, serial, text, tinyint } from "drizzle-orm/mysql-core";
-
-export const summary = mysqlTable('summary', {
-    id: serial('id').primaryKey(),
-    type: tinyint('type').notNull(),
-    description: text('description').notNull(),
-})
