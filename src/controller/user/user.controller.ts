fd7375fc6c9b291b0922f7f8a00b1d712273f36f
DuloeW <duloekeren@gmail.com>
2025-02-28 22:21:48 +0800

    make the respon API user/admin easy to read

--- src/controller/user/user.controller.ts
+++ src/controller/user/user.controller.ts
@@ -2,5 +2,8 @@ import { db } from "../../db/connection.js"
 
 export async function getAllUsers(c: any) {
     const [data] = await db.query('SELECT * FROM users')
-    return c.json({ data })
+    return c.json({ 
+        message: 'Get all users success',
+        data
+     })
 }
